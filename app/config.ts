56cf866a8e5756c7f79951398b18c6a9614b599f
Dng98 <duongpm180598@gmail.com>
2026-07-21 23:43:09 +0700

    update based on my needs

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,5 @@
 const config = {
-  siteName: process.env.SITE_NAME,
+  siteName: process.env.SITE_NAME || "Phạm Gia",
   exampleEmail: process.env.EXAMPLE_EMAIL || "",
   examplePassword: process.env.EXAMPLE_PASSWORD || "",
 };
